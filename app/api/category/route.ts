5d9861a51eda49666a53b2bc854be93805635109
saruni-spec <sarunimaina@gmail.com>
2025-11-12 13:57:00 +0300

    profession

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -4,38 +4,29 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
+    // add an empty string for profession if not provided
+    if (!body.profession) {
+      body.profession = "";
+    }
+
     console.log("Received data:", body);
-    const filteredData = [
-      { prohibited_items: "prohibited_items" },
-      { restricted_items: "restricted_items" },
-      { gifts: "gifts" },
-      { currency_exceeding: "currency_exceeding" },
-      { mobile_device_local_network: "mobile_device_local_network" },
-      { mining_equipment: "mining_equipment" },
-      { good_for_reimportation: "good_for_reimportation" },
-    ];
 
-    const enhancedData = filteredData.map((item, index) => {
-      const key = Object.keys(item)[0] as keyof typeof item;
-      const value = item[key];
-
-      return {
-        id: index + 1,
-        label: value,
-        [key]: value,
-      };
-    });
-
-    console.log(enhancedData);
-
-    console.log("Filtered data:", enhancedData);
-
-    return NextResponse.json(
-      { message: "Success", data: enhancedData },
-      { status: 200 }
+    await fetch(
+      "https://kratest.pesaflow.com/api/customs/passenger-declaration",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
     );
+
+    return NextResponse.json({ message: "Success" }, { status: 200 });
   } catch (error) {
     console.error("Error during create entity process:", error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
+// https://kratest.pesaflow.com/api/customs/passenger-declaration
